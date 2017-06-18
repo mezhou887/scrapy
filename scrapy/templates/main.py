0d9cb8e981b7f99846277b12836f62d0e76c07c5
Zhou Maoen <mezhou887@foxmail.com>
2017-06-18 19:25:06 +0800

    添加pipelines

--- scrapy/templates/main.py
+++ scrapy/templates/main.py
@@ -1,2 +1,18 @@
 from scrapy import cmdline
-cmdline.execute('scrapy crawl sunwz'.split())
+		
+    # http://blog.csdn.net/winterto1990/article/details/52003045
+ 		
+    # templatespider:  爬虫名
+    # template_data： 导出的数据名
+     
+ 		cmdline.execute("scrapy crawl templatespider".split());                                   
+                                                                                             
+    # 持久化一个爬虫，使它能暂停/继续爬取 
+    # 安全地停止爬虫(按 Ctrl-C 或者发送一个信号)                                                 
+    cmdline.execute("scrapy crawl templatespider -s JOBDIR=crawls/templatespider-1".split());
+                                                                                             
+    # 导出Json, xml, csv文件                                                              
+    cmdline.execute("scrapy crawl templatespider -o template_data.json".split());           
+    cmdline.execute("scrapy crawl templatespider -o template_data.jsonlines".split());      
+    cmdline.execute("scrapy crawl templatespider -o template_data.xml".split());            
+    cmdline.execute("scrapy crawl templatespider -o template_data.csv".split());            
--- scrapy/templates/project/module/jsonpipelines.py
+++ scrapy/templates/project/module/jsonpipelines.py
@@ -1,15 +1,21 @@
 import json
 import codecs
 
-class JsonWriterPipeline(object):
+# ITEM_PIPELINES = {                                           
+#     'cnblogs.pipelines.JsonWithEncodingPipeline': 300,
+# }                                                            
+class JsonWithEncodingPipeline(object):
 
     def __init__(self):
-        self.file = codecs.open('sunwz.json', 'w', encoding='utf-8')
+        self.file = codecs.open('sunwz.json', 'w', encoding='utf-8');
 
+    # process_item(item, spider)为每个item pipeline 组件调用，并且需要返回一个scrapy.item.Item实例对象或者抛出一个?scrapy.exceptions.DropItem异常。当抛出异常后该 item 将不会被之后的pipeline 处理。
     def process_item(self, item, spider):
-        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
-        self.file.write(line)
-        return item
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n";
+        self.file.write(line);
+        return item;
 
+    #  当爬虫打开之后被调用: open_spider(spider) 
+    #  当爬虫关闭之后被调用。
     def spider_closed(self, spider):
-        self.file.close()
+        self.file.close();
